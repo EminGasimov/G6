765bc2844a13b63ffa0160272c5b2224f10ad280
FirozKabir65 <firoj.kabir007@gmail.com>
2020-01-09 16:04:26 +0100

    Designed change-password page

--- letstuffgo/resources/views/frontend/include/profile/change_pass.blade.php
+++ letstuffgo/resources/views/frontend/include/profile/change_pass.blade.php
@@ -16,19 +16,55 @@
                                 <a href="#">
                                     <img src="{{ asset('static/website/themes/images/user.png') }}" alt="User Photo" class="image-responsive" style="height: 200px; width: 200px;">
                                 </a>
-                            </div><!-- /.user-photo -->
-                        </div><!-- /.widget -->
+                            </div>
+                        </div>
                         <br>
                         <div class="widget">
                             @include('frontend.include.profile.pro_sidebar')
-                        </div><!-- /.widget -->
-                    </div><!-- /.sidebar -->
-                </div><!-- /.col-* -->
+                        </div>
+                    </div>
+                </div>
 
-
-           
-           
-            </div><!-- /.col-* -->
+                <div class="span9">
+                    <div class="content">
+                        <div class="page-title">
+                            <h3>Change password</h3>
+                        </div>
+                        <hr>
+                        <form action="" method="post">
+                            <div class="change_pass_form">
+                                <div class="row">
+                                    <div class="form-group span6">
+                                        <label>Email</label>
+                                        <input type="email" class="form-control" value="" name="email" required> 
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-group span6">
+                                        <label>Current password</label>
+                                        <input type="password" class="form-control" value="" name="password" required> 
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-group span6">
+                                        <label>New password</label>
+                                        <input type="password" class="form-control" value="" name="n_password" required> 
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="form-group span6">
+                                        <label>Confirm password</label>
+                                        <input type="password" class="form-control" value="" name="c_password" required> 
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="span6">
+                                <button class="btn btn-primary btn-md" type="submit">Update</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 
